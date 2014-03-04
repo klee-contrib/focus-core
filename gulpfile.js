4e36e4d703ac4f04ded5311c8c3fbea2ffac2e84
Pierre Besson <pierre.besson7@gmail.com>
2014-03-04 14:39:11 +0100

    Add the gulp file.

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,21 @@ var gulp = require('gulp');
  
 // include plug-ins
 var jshint = require('gulp-jshint');
- 
+var gulpif = require('gulp-if');
+var coffee = require('gulp-coffee');
+var concat = require('gulp-concat');
+ var gutil = require('gulp-util');
 // JS hint task
 gulp.task('jshint', function() {
   gulp.src('./lib/*/*.js')
     .pipe(jshint())
     .pipe(jshint.reporter('default'));
+});
+
+//
+gulp.task('build', function(){
+  gulp.src(['./lib/helpers/*'])
+    .pipe(gulpif(/[.]coffee$/, coffee())).on('error', gutil.log)
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest('./dest'));
 });
