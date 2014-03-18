85cd9dddceb067fbac5507492b798e019cc60b7f
Pierre <pierre.besson7@gmail.com>
2014-03-18 08:38:09 +0100

    Update gulp file and improve its documentation, separate require for linting into the task, for build.

--- gulpfile.js
+++ gulpfile.js
@@ -2,38 +2,46 @@
 var gulp = require('gulp');
 
 // include plug-ins
-//var jshint = require('gulp-jshint');
 var gulpif = require('gulp-if');
 var coffee = require('gulp-coffee');
 var concat = require('gulp-concat');
 var gutil = require('gulp-util');
-var stylish = require('jshint-stylish');
-var coffeelint = require('gulp-coffeelint');
+
+/**********************
+  Linting files
+**********************/
 
 // JS and coffee lint task
-//gulp.task('lint', function() {
-//  //Js linting.
-//  gulp.src('./lib/*/*.js')
-//    .pipe(jshint())
-//    .pipe(jshint.reporter(stylish));
-//  //Coffee linting
-//  gulp.src('./lib/*/*.coffee')
-//    .pipe(coffeelint())
-//    .pipe(coffeelint.reporter());
-//});
+gulp.task('lint', function() {
+  var jshint = require('gulp-jshint');
+  var stylish = require('jshint-stylish');
+  var coffeelint = require('gulp-coffeelint');
+  //Js linting.
+  gulp.src('./lib/*/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter(stylish));
+  //Coffee linting
+  gulp.src('./lib/*/*.coffee')
+    .pipe(coffeelint())
+    .pipe(coffeelint.reporter());
+});
+
+/**********************
+  Building js files
+**********************/
 
 //Build all the javascripts file.
 gulp.task('browser-build', function() {
   //Build the js file for the browser.
-  gulp.src([ './lib/main.js', 'lib/helpers/custom_exception.coffee','lib/helpers/validators.js','./lib/models/*', 'lib/views/notifications-view.js', 'lib/helpers/post_rendering_helper.js', './lib/helpers/*', './lib/views/*'])
+  gulp.src(['./lib/main.js', 'lib/helpers/custom_exception.coffee', 'lib/helpers/validators.js', './lib/models/*', 'lib/views/notifications-view.js', 'lib/helpers/post_rendering_helper.js', './lib/helpers/*', './lib/views/*'])
     .pipe(gulpif(/[.]coffee$/, coffee())).on('error', gutil.log) //browser deploy
   .pipe(concat('fmk.js'))
     .pipe(gulp.dest('./dist/browser/'))
     .pipe(gulp.dest('./example/app/js/'))
-    //Destination du projet.
-    .pipe(gulp.dest('../../../../UESL_Gimini/Main/Sources/Nantissement.SPA/vendor/'));
+  //Destination du projet.
+  //.pipe(gulp.dest('../../../../UESL_Gimini/Main/Sources/Nantissement.SPA/vendor/'));
 });
-
+//Build all the javascripts for node and unit tests.
 gulp.task('node-build', function() {
   // Build the node sources.
   gulp.src(['./lib/helpers/*'])
@@ -46,36 +54,46 @@ gulp.task('node-build', function() {
     .pipe(gulp.dest('./dist/node/services/'));
 
 });
+//Grouping build tasks.
+gulp.task('build', ['browser-build', 'node-build']);
 
-//All tasks by default.
-gulp.task('build', ['browser-build', 'node-build'/*, 'templates'*/]);
 
-//Gulp build example templates into a template.js file
-//var handlebars = require('gulp-handlebars');
-//var defineModule = require('gulp-define-module');
-//var declare = require('gulp-declare');
-//var concat = require('gulp-concat');
+/**********************
+  Example templates
+**********************/
 
-//gulp.task('templates', function(){
-//  gulp.src(['example/app/templates/*.hbs'])
-//    .pipe(handlebars())
-//    .pipe(defineModule('plain'))
-//    .pipe(declare({
-//      namespace: 'Example.templates'
-//    }))
-//    .pipe(concat('templates.js'))
-//    .pipe(gulp.dest('example/app/js/'));
-//});
+//Gulp build example templates into a template.js file for the example.
+gulp.task('templates', function() {
+  var handlebars = require('gulp-handlebars');
+  var defineModule = require('gulp-define-module');
+  var declare = require('gulp-declare');
 
-//gulp.task('serve', startExpress);
-//function startExpress() {
- 
-//  var express = require('express');
-//  var app = express();
-//  app.use(express.static(__dirname + '/example/app/'));
-//  app.listen(4000);
-//}
+  gulp.src(['example/app/templates/*.hbs'])
+    .pipe(handlebars())
+    .pipe(defineModule('plain'))
+    .pipe(declare({
+      namespace: 'Example.templates'
+    }))
+    .pipe(concat('templates.js'))
+    .pipe(gulp.dest('example/app/js/'));
+});
+
+
+/**********************
+  Local example server
+**********************/
+
+//Start the local server.
+gulp.task('serve', startExpress);
+//Start an express local server in order to be able to serve the example app.
+function startExpress() {
+  var express = require('express');
+  var app = express();
+  app.use(express.static(__dirname + '/example/app/'));
+  app.listen(4000);
+  gutil.log('Express server, serving example at http://localhost:4000');
+}
 
 
 // The default task (called when you run `gulp` from cli)
-gulp.task('default', ['build'/*, 'serve'*/]);
+gulp.task('default', ['build', 'templates', 'serve']);
