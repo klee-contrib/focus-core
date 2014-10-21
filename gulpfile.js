b5dd5c228ba86d01425e453a16cffcef787c34b9
Pierre <pierre.besson7@gmail.com>
2014-10-21 21:21:06 +0200

    Add the eslint as part of build

--- gulpfile.js
+++ gulpfile.js
@@ -20,14 +20,43 @@ gulp.task('lint', function() {
   var stylish = require('jshint-stylish');
   var coffeelint = require('gulp-coffeelint');
   //Js linting.
-  gulp.src('./lib/*/*.js')
+  gulp
+    .src('./lib/*/*.js')
     .pipe(jshint())
     .pipe(jshint.reporter(stylish));
   //Coffee linting
-  gulp.src('./lib/*/*.coffee')
+  gulp
+    .src('./lib/*/*.coffee')
     .pipe(coffeelint())
     .pipe(coffeelint.reporter());
 });
+
+//Eslint
+
+gulp.task('eslint', function() {
+  //gulp eslint 2>lint/lintErrors.txt
+  var eslint = require('gulp-eslint');
+  var options = {
+       "globals":{
+        "jQuery": false,
+        "$": true,
+        "require": true,
+        "Backbone": true,
+        "Fmk": true,
+        "_": true,
+        "Promise": true
+    }
+  };
+  var format;
+  gulp
+    .src('./lib/*/*.js')
+    .pipe(eslint(options))
+    //.pipe(eslint.format(undefined, process.stdout))
+    //.pipe(eslint.failOnError())
+     .pipe(eslint.formatEach(format, process.stderr))
+     //.on('error', gutil.log);
+});
+
 
 /*********************
 Documentation generation using jsDoc
@@ -48,10 +77,10 @@ gulp.task('jsdoc', function() {
 **********************/
 gulp.task('css-build', function() {
   gulp.src('./lib/styles/*.css')
-  .pipe(concat('fmk.css'))
-  .pipe(gulp.dest('./example/app/css/'))
-  .pipe(gulp.dest('./dist/browser/'))
-  .pipe(gulp.dest(buildConf.spaDirectory));
+    .pipe(concat('fmk.css'))
+    .pipe(gulp.dest('./example/app/css/'))
+    .pipe(gulp.dest('./dist/browser/'))
+    .pipe(gulp.dest(buildConf.spaDirectory));
 });
 /**********************
   Building js files
@@ -80,7 +109,7 @@ gulp.task('browser-build', ['infos', 'templates'], function() {
     './lib/helpers/util_helper.js',
     './lib/helpers/url_helper.js',
     "./lib/helpers/backbone_notification.js",
-      //All helpers
+    //All helpers
     "./lib/helpers/post_rendering_builder.js",
     "./lib/helpers/post_rendering_helper.js",
     "./lib/helpers/ustom_exception.coffee",
@@ -114,9 +143,9 @@ gulp.task('browser-build', ['infos', 'templates'], function() {
   .pipe(concat('fmk.js'))
     .pipe(gulp.dest('./dist/browser/'))
     .pipe(gulp.dest('./example/app/js/'))
-    //Current project destination.
-    //.pipe(gulp.dest('../SPA-skeleton/vendor'));
-    .pipe(gulp.dest(buildConf.spaDirectory));
+  //Current project destination.
+  //.pipe(gulp.dest('../SPA-skeleton/vendor'));
+  .pipe(gulp.dest(buildConf.spaDirectory));
 });
 //Build all the javascripts for node and unit tests.
 gulp.task('node-build', function() {
@@ -148,16 +177,16 @@ gulp.task('templates', function() {
     .pipe(gulp.dest('lib/templates/'));
 });
 
-gulp.task('templateHelper', function(){
+gulp.task('templateHelper', function() {
   //Build the js file for the browser.
-    gulp.src([
-      './lib/templates/helpers/common/*.js',
-      './lib/templates/helpers/common/*.coffee',
-      './lib/templates/helpers/*.js',
-      './lib/templates/helpers/*.coffee'
-    ])
+  gulp.src([
+    './lib/templates/helpers/common/*.js',
+    './lib/templates/helpers/common/*.coffee',
+    './lib/templates/helpers/*.js',
+    './lib/templates/helpers/*.coffee'
+  ])
     .pipe(gulpif(/[.]coffee$/, coffee())).on('error', gutil.log) //browser deploy
-    .pipe(concat('templateHelper.js'))
+  .pipe(concat('templateHelper.js'))
     .pipe(gulp.dest('./lib/templates/'));
 });
 //Grouping build tasks.
@@ -194,18 +223,18 @@ gulp.task('templatesExample', function() {
 
 
 //Build a file from the package.json in order to have it present in the fmk.js file.
-gulp.task('infos', function () {
-    
-    var fs = require('fs');
-    var pajson = require('./package.json');
-  var string = '/* name: ' + pajson.name + ' , version: ' + pajson.version + ' description: ' + pajson.description + "*/ \n (function initialization(container) {var fmk = container.Fmk || {};fmk.name = '"+ pajson.name+"';fmk.version = '"+ pajson.version+"';container.Fmk = fmk;})(typeof module === 'undefined' && typeof window !== 'undefined' ? window : exports);";
+gulp.task('infos', function() {
+
+  var fs = require('fs');
+  var pajson = require('./package.json');
+  var string = '/* name: ' + pajson.name + ' , version: ' + pajson.version + ' description: ' + pajson.description + "*/ \n (function initialization(container) {var fmk = container.Fmk || {};fmk.name = '" + pajson.name + "';fmk.version = '" + pajson.version + "';container.Fmk = fmk;})(typeof module === 'undefined' && typeof window !== 'undefined' ? window : exports);";
   fs.writeFile("lib/infos.js", string, function(err) {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log("The infos.js file was created.");
-        }
-    });
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("The infos.js file was created.");
+    }
+  });
 });
 
 /**********************
