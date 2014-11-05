de23d7dd6983cbf806bf766765f7f9226aa2ba45
Pierre Besson <pierre.besson7@gmail.com>
2014-11-05 18:21:28 +0100

    Update the publish.

--- publish.sh
+++ publish.sh
@@ -1,6 +1,7 @@
 gulp build
-git add dist
-git add example
+git add -f dist
+git add -f example
+git add lib/infos.js
 git commit -m 'Add the new version distribution'
 npm version patch
 git push origin master
