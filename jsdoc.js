b98e8ae98ce79b30a83cc206f6b315992a52d22b
fconstantin <francois.constantin@kleegroup.com>
2015-01-13 15:59:27 +0100

    #68 Comment ConsultEditView

--- jsdoc.js
+++ jsdoc.js
@@ -2,6 +2,7 @@
 {
     "source": {
         "include": [
+            "lib/views/consult-edit-view.js",
             "lib/views/core-view.js",
             "lib/views/index.js",
             "lib/views/search-view.js"]
