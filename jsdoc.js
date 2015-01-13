7e25d11ee4d7407ca9fac202066de1b7f0611111
fconstantin <francois.constantin@kleegroup.com>
2015-01-13 13:48:49 +0100

    #68 Comment CoreView and SearchView

---
+++ jsdoc.js
@@ -0,0 +1,9 @@
+/** JSDoc configuration */
+{
+    "source": {
+        "include": [
+            "lib/views/core-view.js",
+            "lib/views/index.js",
+            "lib/views/search-view.js"]
+    }
+}
