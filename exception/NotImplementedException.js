875dba239c6eccb5b4ad009e64db92c6ecdd92b2
Pierre Besson <pierre.besson7@gmail.com>
2015-03-04 11:44:51 +0100

    [exception] Add exceptions to the focus fmk.

---
+++ exception/NotImplementedException.js
@@ -0,0 +1,16 @@
+var CustomException = require('CustomException');
+/**
+ * Class standing for the NotImplemented exceptions.
+ */
+class NotImplementedException extends CustomException{
+  /**
+   * Exception constructor..
+   * @param messgae {string} - Exception message.
+   * @param options {object} - Object to add to the exception.
+   */
+  constructor(message, options){
+    super("NotImplementedException", message, options);
+  }
+}
+
+module.expôrts = NotImplementedException;
