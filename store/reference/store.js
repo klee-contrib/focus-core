c29070dc37fb4e289058777d1b13b35f8afa6b4c
Pierre <pierre.besson7@gmail.com>
2015-03-25 02:38:58 +0100

    [reference] Store add method to load references.

--- store/reference/store.js
+++ store/reference/store.js
@@ -5,11 +5,24 @@ var buildDefinition = require('./definition');
  * Class standing for the reference store.
  */
 class ReferenceStore extends CoreStore {
+
+
   constructor(conf){
     conf = conf || {};
     conf.definition = conf.definition || buildDefinition();
     super(conf);
   }
+  getReference(names){
+    var refs = {};
+    names.map((name)=>{
+      if(this.data.has(name)){
+        refs[name] = this.data.get(name);
+      }
+    });
+    return {references: this.data.toJS()};
+  }
+  setReference(){}
+
 }
 
 module.exports = ReferenceStore;
