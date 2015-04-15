8d4eb75a68a93e39c7bf290186b88afcb3bd09da
Pierre Besson <pierre.besson7@gmail.com>
2015-04-15 17:26:44 +0200

    [type] Add mandatory in type props checking

--- component/types.js
+++ component/types.js
@@ -7,17 +7,30 @@ var isArray = require('lodash/lang/isArray');
 /**
  * Expose a React type validation for the component properties validation.
  * @see http://facebook.github.io/react/docs/reusable-components.html
- * @param  {string} type - String or array of the types to use.
- * @return {object} The corresponding react type.
+ * @param   {string} type - String or array of the types to use.
+ * @param   {boolean} isRequired - Defines if the props is mandatory.
+ * @returns {object} The corresponding react type.
  */
-module.exports = function(type){
+module.exports = function(type, isRequired){
   var isStringType = isString(type);
   if(!isStringType && !isArray(type)){
     throw new Error('The type should be a string or an array');
   }
+  //Container for the propTypes.
+  var propTypeToReturn;
+  //Array case.
   if(isStringType){
-    return React.PropTypes[type];
+    propTypeToReturn = React.PropTypes[type];
+  }else {
+    propTypeToReturn = React.PropTypes.oneOfType(
+      type.map(
+        (type)=>{
+          return React.PropTypes[type];
+    }));
   }
-  return React.PropTypes.oneOfType(type.map((type)=>{return React.PropTypes[type]}));
-
+  //Mandatory case
+  if(isRequired){
+    propTypeToReturn = propTypeToReturn.isRequired;
+  }
+  return propTypeToReturn;
 };
