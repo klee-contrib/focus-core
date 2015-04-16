3b10465bcdc5f9b4b16e4b2e499e0973cb6b391b
Pierre Besson <pierre.besson7@gmail.com>
2015-04-16 11:07:24 +0200

    [exception]

--- exception/CustomException.js
+++ exception/CustomException.js
@@ -5,7 +5,9 @@ class CustomException extends Error{
   constructor(name, message, options){
     super(message);
     this.name = name;
+    this.message = message;
     this.options = options;
+    this.log();
   }
   /**
    * Log the exception in the js console.
