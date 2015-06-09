2d836425e9a2057c71f81cd91ca0da5ca01cd815
Pierre Besson <pierre.besson7@gmail.com>
2015-06-09 14:06:29 +0200

    [router] Add error messages when the route is unknown

--- router/index.js
+++ router/index.js
@@ -1,6 +1,39 @@
 var render = require('../application/render');
 var Backbone = require('backbone');
+var ArgumentNullException = require('../exception/ArgumentNullException');
+var message = require('../message');
+var userHelper = require('../user');
 module.exports = Backbone.Router.extend({
+  noRoleRoute: 'home',
+  route(route, name, callback) {
+    var router = this;
+    if (!callback){
+      callback = this[name];
+    }
+    if(callback === undefined || callback === null){
+      throw new ArgumentNullException(`The route callback seems to be undefined, please check your router file for your route: ${name}`);
+    }
+    var customWrapperAroundCallback = ()=>{
+      var currentRoute = route;
+      //The default route is the noRoleRoute by default
+      if(currentRoute === ''){
+        currentRoute = router.noRoleRoute;
+      }
+      var routeName = '';//siteDescriptionBuilder.findRouteName(currentRoute);
+      var routeDescciption = {roles: ['DEFAULT_ROLE']};//siteDescriptionBuilder.getRoute(routeName);
+      if((routeDescciption === undefined && currentRoute !== '') || !userHelper.hasRole(routeDescciption.roles)){
+        message.addErrorMessage('application.noRights');
+        return Backbone.history.navigate('', true);
+      }else {
+        //Rendre all the notifications in the stack.
+        backboneNotification.renderNotifications();
+      }
+      //console.log('routeObject', siteDescriptionBuilder.getRoute(n));
+      callback.apply(router, arguments);
+
+  };
+    return Backbone.Router.prototype.route.call(this, route, name, customWrapperAroundCallback);
+  },
   /**
    * Render the compoennt into the page content.
    */
