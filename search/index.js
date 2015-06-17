f5d0df549ff631b8de2853316a201bc253958be4
Pierre Besson <pierre.besson7@gmail.com>
2015-06-17 11:28:47 +0200

    [query-scope] Add a method to dispatch the new value of query and scope.

--- search/index.js
+++ search/index.js
@@ -1,3 +1,16 @@
+let dispatcher = require('../dispatcher');
 module.exports = {
-    builtInStore: require('./built-in-store')
+    builtInStore: require('./built-in-store'),
+    /*
+    * Dispatch the change of the query value, with the callerId information.
+    */
+    changeQuery(queryValue, callerId){
+      dispatcher.handleViewAction({data: {query: queryValue}, type: 'update', callerId: callerId});
+    },
+    /*
+    * Dispatch the change of the scope value, with the callerId information.
+    */
+    changeScope(scopeValue, callerId){
+      dispatcher.handleViewAction({data: {scope: scopeValue}, type: 'update', callerId: callerId});
+    }
 };
