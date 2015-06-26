2fdaf4a3b4fdd2e184c6ea1cded9dadf48d55452
Pierre Besson <pierre.besson7@gmail.com>
2015-06-26 13:16:20 +0200

    [search-action] Add questions?

--- search/search-action.js
+++ search/search-action.js
@@ -5,10 +5,19 @@ module.exports = function(config){
     let {scope, query} = options.criteria; //Cannot be read from the store ?
     if(scope === ALL){
       //Call the search action.
+      options.service.scope(options);
     }else{
+      if(options.scroll){ //Maybe rename pagination or something like that.
+        options.service.unScope(options).then((response)=>{
+            // Read the previous data from options.previous;
+            return response;
+        });
+        //Read the totalCount
+
+      }
       //The search is unscoped.
 
-      
+
     }
   };
-}
+};
