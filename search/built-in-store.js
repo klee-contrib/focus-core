fac147059ec490f464fe690a633ab4f5f4fa3059
Pierre Besson <pierre.besson7@gmail.com>
2015-06-26 12:29:18 +0200

    [search] fix built in store

--- search/built-in-store.js
+++ search/built-in-store.js
@@ -1,5 +1,5 @@
 let AdvancedSearchStore = require('../store/search/advanced-search');
-let QuickSearchStore = require('../store/search/advanced-search');
+let QuickSearchStore = require('../store/search/quick-search');
 
 module.exports = {
     quickSearchStore: new QuickSearchStore(),
