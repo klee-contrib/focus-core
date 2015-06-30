0aad5bc099f0063e5014b98e965ca7cb0c13b426
Stanislas Bernard <bernardstanislas@gmail.com>
2015-06-30 18:13:16 +0200

    [search-store] Remove getter

--- store/search/search-store.js
+++ store/search/search-store.js
@@ -1,12 +1,12 @@
 let CoreStore = require('../CoreStore');
 /**
- * Class standing for all advanced search information.
- * The state should be the complete state of the page.
- */
+* Class standing for all advanced search information.
+* The state should be the complete state of the page.
+*/
 class SearchStore extends CoreStore{
-  get value(){
-    return this.data.toJS();
-  }
+    getValue() {
+        return this.data.toJS();
+    }
 }
 
 module.exports = SearchStore;
