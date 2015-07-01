c4d9844bd46f41ce8aea2c21eddc4d4b776db447
Pierre Besson <pierre.besson7@gmail.com>
2015-07-01 19:48:47 +0200

    [user] Fix built in store

--- user/index.js
+++ user/index.js
@@ -1,5 +1,5 @@
 var userBuiltInStore = require('./built-in-store');
-var userStoreInstance = userBuiltInStore();
+
 var isArray = require('lodash/lang/isArray');
 var intersection = require('lodash/array/intersection');
 var dispatcher = require('../dispatcher');
@@ -9,9 +9,12 @@ module.exports = {
   builtInStore: userBuiltInStore,
   hasRole(role){
     role = isArray(role) ? role : [role];
-    return intersection(role, userStoreInstance.getRoles()).length > 0;
+    return intersection(role, userBuiltInStore.getRoles()).length > 0;
   },
   setRoles(roles){
     dispatcher.handleViewAction({data: {roles: roles}, type: 'update'});
+  },
+  getRoles(){
+    return userBuiltInStore.getRoles();
   }
 };
