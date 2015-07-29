944da39ae84f8047e0a4d4a062c07d69ac1bdd63
Pierre Besson <pierre.besson7@gmail.com>
2015-07-30 00:08:31 +0200

    [tests] Add number test.

---
+++ definition/validator/__tests__/number-test.js
@@ -0,0 +1,39 @@
+/*global jest, expect*/
+// __tests__/container-test.js
+jest.dontMock('../number');
+require('../../../test/dontMock');
+
+describe('### validator number', function() {
+  it('number should be valid', function() {
+    var numberValidation = require('../number');
+    expect(numberValidation(1)).toEqual(true);
+  });
+  it('null should be valid', function() {
+    var numberValidation = require('../number');
+    expect(numberValidation(null)).toEqual(true);
+  });
+  it('undefined should be valid', function() {
+    var numberValidation = require('../number');
+    expect(numberValidation(undefined)).toEqual(true);
+  });
+  it('string should be invalid', function() {
+    var numberValidation = require('../number');
+    expect(numberValidation('ABCD')).toEqual(false);
+  });
+  it('string should be invalid', function() {
+    var numberValidation = require('../number');
+    expect(numberValidation('ABCD')).toEqual(false);
+  });
+  it('number min should be false when number inferior to min', function() {
+    var numberValidation = require('../number');
+    expect(numberValidation('6', {min: 7})).toEqual(false);
+  });
+  it('number min should be true when number superior to min', function() {
+    var numberValidation = require('../number');
+    expect(numberValidation('7', {min: 6})).toEqual(true);
+  });
+  it('number max should be true when number inferior to max', function() {
+    var numberValidation = require('../number');
+    expect(numberValidation('7', {max: 7})).toEqual(true);
+  });
+});
