cb70e429bf5f635de2c1f282d7fd49f6097af18c
Pierre Besson <pierre.besson7@gmail.com>
2015-08-21 23:16:42 +0200

    [eslint] clean definition/entity/formatter

--- src/definition/formatter/number.js
+++ src/definition/formatter/number.js
@@ -1,8 +1,14 @@
-let numeral = require('numeral');
+const numeral = require('numeral');
 
 const DEFAULT_FORMAT = '0,0';
 
 module.exports = {
+    /**
+     * Format a number using a given format.
+     * @param  {number} number - The number to format.
+     * @param  {string} format - The format to transform.
+     * @return {string} - The formated number.
+     */
     format(number, format) {
         format = format || DEFAULT_FORMAT;
         return numeral(number).format(format);
