24f022afde2da07dc9dac4093b1df59c35f87be6
Pierre Besson <pierre.besson7@gmail.com>
2015-08-24 08:59:07 +0200

    [action-builder]

--- src/exception/ArgumentInvalidException.js
+++ src/exception/ArgumentInvalidException.js
@@ -1,15 +1,15 @@
-var CustomException = require('./CustomException');
-/** 
- * Class standing for the NotImplemented exceptions.
- */
+const CustomException = require('./CustomException');
+/**
+* Class standing for the NotImplemented exceptions.
+*/
 class ArgumentInvalidException extends CustomException {
 	/**
-	 * Exception constructor..
-	 * @param messgae {string} - Exception message.
-	 * @param options {object} - Object to add to the exception.
-	 */
+	* Exception constructor..
+	* @param messgae {string} - Exception message.
+	* @param options {object} - Object to add to the exception.
+	*/
 	constructor(message, options) {
-		super("ArgumentInvalidException", message, options);
+		super('ArgumentInvalidException', message, options);
 	}
 }
 
