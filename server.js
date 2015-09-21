a370b223e64309e696273dfdf2c42666f4939c92
Pierre Besson <pierre.besson7@gmail.com>
2015-09-21 14:20:29 +0200

    [webpack] Add dev server.

---
+++ server.js
@@ -0,0 +1,13 @@
+const webpack = require('webpack');
+const WebpackDevServer = require('webpack-dev-server');
+const config = require('./webpack.config');
+
+new WebpackDevServer(webpack(config), {
+    publicPath: config.output.publicPath,
+    historyApiFallback: true
+}).listen(3000, 'localhost', (err, result)=>{
+    if (err) {
+        console.log(err);
+    }
+    console.log('Listening at localhost:3000');
+});
