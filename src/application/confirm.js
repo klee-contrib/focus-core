0d135e7b094cd696d2933a27dbab9b71b739205f
Stan <bernardstanislas@gmail.com>
2015-12-14 14:57:00 +0100

    [confirm] Fix #264

--- src/application/confirm.js
+++ src/application/confirm.js
@@ -5,20 +5,21 @@ import {handleViewAction as dispatch} from '../dispatcher';
  * @param  {string | numver | component} ContentComponent - The component to display in the conform message.
  * @return {Promise}Confirm is a promise in order to be able to provide success and error callbacks.
  */
-export default function confirm(ContentComponent){
+export default function confirm(ContentComponent, props) {
     return new Promise((resolve, reject) => {
         dispatch({
             data: {
-                confirmConfig:{
+                confirmConfig: {
                     isVisible: true,
                     Content: ContentComponent,
-                    handleCancel(err){
+                    contentProps: props,
+                    handleCancel(err) {
                         dispatch({data: {confirmConfig: {isVsible: false, Content: null}}, type: 'update'});
                         //Maybe there is a little async problem.
                         // We could listen to the store once on the change it is time to call resolve.
                         reject(err);
                     },
-                    handleConfirm(data){
+                    handleConfirm(data) {
                         dispatch({data: {confirmConfig: {isVsible: false, Content: null}}, type: 'update'});
                         resolve(data);
                     }
