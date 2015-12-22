f1c8488a6fa85433e62244a27516736f2fad2b96
Damien Frikha <JabX@users.noreply.github.com>
2015-12-22 15:21:34 +0100

    headerSize -> canDeploy.

--- src/store/application/definition.js
+++ src/store/application/definition.js
@@ -3,7 +3,7 @@
 * @return {object} - The cartridge component.
 */
 module.exports = () => {
-    return ['summaryComponent', 'barContentLeftComponent', 'barContentRightComponent', 'cartridgeComponent', 'actions', 'mode', 'route', 'confirmConfig', 'headerSize']
+    return ['summaryComponent', 'barContentLeftComponent', 'barContentRightComponent', 'cartridgeComponent', 'actions', 'mode', 'route', 'confirmConfig', 'canDeploy']
     .reduce((def, node) => {
         def[node] = node;
         return def;
