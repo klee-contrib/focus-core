dae5566c49bd5e5d6caac06e58e4edaf3f961d4a
Stan <bernardstanislas@gmail.com>
2016-01-04 16:33:23 +0100

    Cleanup

--- publish.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-npm version patch
-gulp build
-git add package.json
-git add -f dist
-git add -f example
-git commit -m 'Add the new version distribution'
-git push origin master
-git push origin --tags
--- server.js
+++
@@ -1,3 +0,0 @@
-const server = require('webpack-focus').server;
-const focusCoreConf = require('./focus-core-webpack.config');
-server(focusCoreConf);
