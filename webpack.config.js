2a0f0f598cb18eee14f83bd4150c47e27bd08c93
Stan <bernardstanislas@gmail.com>
2016-01-04 16:33:38 +0100

    Cleaner webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,14 @@
-const devConfBuilder = require('webpack-focus').devConfig;
-const focusComponentsConf = require('./focus-core.webpack.config');
-const devConf = devConfBuilder(focusComponentsConf);
-module.exports = devConf;
+const configBuilder = require('webpack-focus').configBuilder;
+const customConfig = {
+    externals: {
+        react: 'React',
+        'react-dom': 'ReactDOM',
+        moment: 'moment',
+        jquery: 'jQuery',
+        backbone: 'Backbone',
+        numeral: 'numeral',
+        'i18next-client': 'i18n'
+    }
+};
+
+module.exports = configBuilder(customConfig);
