bcf475cc89a981cc30ee8e42dbdd6bf109fea4ea
Pascal LEPAISANT <pascal.lepaisant@kleegroup.com>
2016-02-03 15:46:38 +0100

    Error thrown when inconstency between totalCount and datalist

--- src/list/load-action/parser.js
+++ src/list/load-action/parser.js
@@ -5,6 +5,9 @@ module.exports = (data, context) => {
     if(context.isScroll){
         dataList = [...context.dataList, ...data.dataList];
     }
+    if ((dataList.length === 0) && (totalCount > 0)){
+        throw new Error('totalCount must be equal to zero when no data are returned!!');
+    }
     return ({
         dataList: dataList,
         totalCount: totalCount,
