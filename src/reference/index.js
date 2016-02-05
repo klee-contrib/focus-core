55ba23f1d8ce7d9a5521785af5b29836686dfa5b
Stan <bernardstanislas@gmail.com>
2016-02-05 11:23:53 +0100

    Fix reference store issue

--- src/reference/index.js
+++ src/reference/index.js
@@ -2,5 +2,5 @@ module.exports = {
   config: require('./config'),
   builder: require('./builder'),
   builtInAction: require('./built-in-action'),
-  builtInStore: require('./built-in-store')
+  storeGetter: require('./built-in-store')
 };
