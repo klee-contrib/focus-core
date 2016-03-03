6c1839370b8bf9553aa0466ee3b3c5f9749f229c
Thomas <tgallon@gmail.com>
2016-03-03 10:52:07 +0100

    add numeral formatter init languages

--- src/definition/formatter/number.js
+++ src/definition/formatter/number.js
@@ -1,16 +1,23 @@
-const numeral = require('numeral');
+import numeral from 'numeral';
 
 const DEFAULT_FORMAT = '0,0';
 
-module.exports = {
+//TODO change numeral lib and regroup initializers
+export function language(key, conf) {
+    return numeral.language(key, conf);
+};
+
+export default {
+
     /**
-     * Format a number using a given format.
-     * @param  {number} number - The number to format.
-     * @param  {string} format - The format to transform.
-     * @return {string} - The formated number.
-     */
+    * Format a number using a given format.
+    * @param  {number} number - The number to format.
+    * @param  {string} format - The format to transform.
+    * @return {string} - The formated number.
+    */
     format(number, format) {
         format = format || DEFAULT_FORMAT;
         return numeral(number).format(format);
     }
+
 };
