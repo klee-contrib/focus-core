867d6a49c5d5d53cbe3149a48ec662f88da535c9
GuenoleK <kikabouguenole@gmail.com>
2016-05-19 16:18:47 +0200

    Adding react-router navigations

--- src/history/index.js
+++ src/history/index.js
@@ -1,3 +1,13 @@
 import {history} from 'backbone';
+import React from 'react';
+import {browserHistory, Link} from 'react-router';
 
-export default history;
+function navigate(url) {
+    browserHistory.push(url);
+}
+
+export default {
+    navigate,
+    Link,
+    history
+}
