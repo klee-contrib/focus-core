4b388d98012e3853010d727c13a659e39daf88cc
GuenoleK <kikabouguenole@gmail.com>
2016-05-25 19:40:41 +0200

    Updating navigate for backbone case

--- src/history/index.js
+++ src/history/index.js
@@ -4,18 +4,27 @@ let _navigate, _back;
 export const setNavigationFunctions = (navigate, back) => {
     _navigate = navigate;
     _back = back;
+    window._navigate = navigate;
+    window._back = back;
 }
 
 export const navigate = (...args) => {
-
     if(!_navigate){
-        throw new Error('react-router or backbone URL Navigation was badly given in the setNavigationFunctions()')
+        if(!window._navigate) {
+            throw new Error('react-router or backbone URL Navigation was badly given in the setNavigationFunctions()')
+        }
+        else {
+            window._navigate(...args);
+        }
     }
-    _navigate(...args);
+    else {
+        _navigate(...args);
+    }
 }
 
 export const back = (...args) => {
     if(!_back){
+        window._back(...args);
         throw new Error('react-router or backbone Previous Page Navigation was badly given in the setNavigationFunctions()')
     }
     _back(...args);
