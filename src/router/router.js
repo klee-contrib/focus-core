f0d187c195c9aeb6a12959710c394aad52cb959b
GuenoleK <kikabouguenole@gmail.com>
2016-05-26 10:02:43 +0200

    Adding router when Backbone is given

---
+++ src/router/router.js
@@ -0,0 +1,25 @@
+let _Router;
+
+export const setRouterFunction =(Router) => {
+    _Router = Router;
+};
+
+export const getRouterFunction = (...args) => {
+    if (!_Router) {
+        if(!window._Router) {
+            throw new Error(`
+                Router: you need to define your Backbone router using focus-core/router/router setRouterFunction'
+                // Don't forget to add backbone to your dependency.
+                Example :
+                import Backbone from 'backbone';
+                setRouterFunction(Backbone.Router)
+                `);
+        }
+        else {
+            return window._Router
+        }
+    }
+    else {
+        return _Router
+    }
+};
