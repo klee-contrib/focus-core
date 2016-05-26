4e4d8a38b9dcb5af9cd0f9e1c61b7d58a16b0850
GuenoleK <kikabouguenole@gmail.com>
2016-05-26 17:31:50 +0200

    Updating the call navigate context

--- src/history/index.js
+++ src/history/index.js
@@ -4,17 +4,15 @@ let _navigate, _back;
 export const setNavigationFunctions = (navigate, back) => {
     _navigate = navigate;
     _back = back;
-    window._navigate = navigate;
-    window._back = back;
 }
 
 export const navigate = (...args) => {
     if(!_navigate){
-        if(!window._navigate) {
+        if(!window.Backbone) {
             throw new Error('react-router or backbone URL Navigation was badly given in the setNavigationFunctions()')
         }
         else {
-            window._navigate(...args);
+            window.Backbone.history.navigate(...args);
         }
     }
     else {
@@ -24,7 +22,6 @@ export const navigate = (...args) => {
 
 export const back = (...args) => {
     if(!_back){
-        window._back(...args);
         throw new Error('react-router or backbone Previous Page Navigation was badly given in the setNavigationFunctions()')
     }
     _back(...args);
