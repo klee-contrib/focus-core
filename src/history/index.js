bf1b4578508f390b16594f471d49eadad2b474ba
Pierre Besson <pierre.besson7@gmail.com>
2016-05-31 19:55:22 +0200

    [router] Remove backbone dependency and abstract navigation functions
    * Adding react-router dependency
    
    * Adding react-router navigations
    
    * Adding some functions
    
    * Adding new way to get react-router or backbone on history
    
    * Update to make history works correctly
    
    * Minor update
    
    * Migrating from backbone to react-router. Preparing the field
    
    * Temporary file to have backbone.history
    
    * Minor update
    
    * Updating the focus router
    
    * Updating navigate for backbone case
    
    * Adding router when Backbone is given
    
    * Adjusting to have the backbone navigation
    
    * Removing backbone references
    
    * Adding react-router dependency
    
    * Adding react-router navigations
    
    * Adding some functions
    
    * Adding new way to get react-router or backbone on history
    
    * Update to make history works correctly
    
    * Minor update
    
    * Migrating from backbone to react-router. Preparing the field
    
    * Temporary file to have backbone.history
    
    * Minor update
    
    * Updating the focus router
    
    * Updating navigate for backbone case
    
    * Adding router when Backbone is given
    
    * Adjusting to have the backbone navigation
    
    * Removing backbone references
    
    * Correctly updating the router
    
    * Fixing package.jsons
    
    * Removing require to use imports in router index.js
    
    * Updating the call navigate context
    
    * Updating the history to works correctly with backbone
    
    * Updating the router for backbone
    
    * Adding the start function for backbone
    
    * Updating the router to work without previous backbone router definition
    
    * Removing backbone dependency from focus-core

--- src/history/index.js
+++ src/history/index.js
@@ -1,3 +1,29 @@
-import {history} from 'backbone';
+let _navigate, _back, _start;
 
-export default history;
+// Define the navigation functions depending on backbone or react-router
+export const setNavigationFunctions = (navigate, back, start) => {
+    _navigate = navigate;
+    _back = back;
+    _start = start;
+}
+
+export const navigate = (...args) => {
+    if(!_navigate) {
+        throw new Error('react-router or backbone URL Navigation was badly given in the setNavigationFunctions()')
+    }
+    _navigate(...args);
+}
+
+export const back = (...args) => {
+    if(!_back){
+        throw new Error('react-router or backbone Previous Page Navigation was badly given in the setNavigationFunctions()')
+    }
+    _back(...args);
+}
+
+export const start = (...args) => {
+    if(!_start) {
+        throw new Error('Backbone start router was badly given in the setNavigationFunctions()')
+    }
+    _start(...args);
+}
