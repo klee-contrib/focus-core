fafa4a6b9cf13a5fa6a509731c5bab6c3be0632a
Damien Frikha <JabX@users.noreply.github.com>
2016-08-31 10:20:44 +0200

    Fix ReferenceStore recreation.

--- src/reference/built-in-store.js
+++ src/reference/built-in-store.js
@@ -1,9 +1,14 @@
 const ReferenceStore = require('../store/reference');
 
+let ref;
+
 /**
 * Built the store in order to the .
 * @return {ReferenceStore} - An instanciated reference store.
 */
-module.exports = function() {
-    return new ReferenceStore();
-}
+module.exports = function () {
+    if (!ref) {
+        ref = new ReferenceStore();
+    }
+    return ref;
+};
