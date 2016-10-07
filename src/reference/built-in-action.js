58ebc37681c6b0b60632c352ac82ab105baaff36
JohnnP <jonathan.pujol12@gmail.com>
2016-10-07 15:21:54 +0200

    Add option to skip cache

--- src/reference/built-in-action.js
+++ src/reference/built-in-action.js
@@ -6,12 +6,12 @@ var dispatcher = require('../dispatcher');
  * @param {array} referenceNames - An array which contains the name of all the references to load.
  * @returns {Promise} - The promise of loading all the references.
  */
-function builtInReferenceAction(referenceNames) {
+function builtInReferenceAction(referenceNames, skipCache) {
     return () => {
         if(!referenceNames) {
             return undefined;
         }
-        return Promise.all(loadManyReferenceList(referenceNames))
+        return Promise.all(loadManyReferenceList(referenceNames, skipCache))
         .then(function successReferenceLoading(data) {
             //Rebuilt a constructed information from the map.
             const reconstructedData = data.reduce((acc,item) => {acc[item.name] = item.dataList; return acc;}, {})
