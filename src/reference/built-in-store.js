c66aacc12ae538576674be5327b5524a1d078742
JohnnP <jonathan.pujol12@gmail.com>
2017-01-11 15:56:11 +0100

    Correction wrong behaviour

--- src/reference/built-in-store.js
+++ src/reference/built-in-store.js
@@ -1,7 +1,14 @@
 import ReferenceStore from '../store/reference'
 
+
+let ref ;
 /**
 * Built the store in order to the .
 * @return {ReferenceStore} - An instanciated reference store.
 */
-export default new ReferenceStore();
+export default () => {
+	if(!ref){
+		ref = new ReferenceStore();
+	}
+	return ref;
+};
