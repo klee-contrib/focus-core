d64b43821bfe1421d552fdb72c4bd5a60fed1b9b
Hartorn <bazirehoussin@gmail.com>
2017-06-14 14:31:16 +0200

    Fixing test unit config and import

--- src/definition/validator/__tests__/number-test.js
+++ src/definition/validator/__tests__/number-test.js
@@ -1,42 +1,34 @@
 /*global expect, describe, it*/
 // __tests__/container-test.js
+import numberValidation from '../number';
 
 
-describe('### validator number', ()=>{
-    it('number should be valid', ()=>{
-        const numberValidation = require('../number');
+describe('### validator number', () => {
+    it('number should be valid', () => {
         expect(numberValidation(1)).to.equal(true);
     });
-    it('null should be valid', ()=>{
-        const numberValidation = require('../number');
+    it('null should be valid', () => {
         expect(numberValidation(null)).to.equal(true);
     });
-    it('undefined should be valid', ()=>{
-        const numberValidation = require('../number');
+    it('undefined should be valid', () => {
         expect(numberValidation(undefined)).to.equal(true);
     });
-    it('string should be invalid', ()=>{
-        const numberValidation = require('../number');
+    it('string should be invalid', () => {
         expect(numberValidation('ABCD')).to.equal(false);
     });
-    it('string should be invalid', ()=>{
-        const numberValidation = require('../number');
+    it('string should be invalid', () => {
         expect(numberValidation('ABCD')).to.equal(false);
     });
-    it('number min should be false when number inferior to min', ()=>{
-        const numberValidation = require('../number');
-        expect(numberValidation('6', {min: 7})).to.equal(false);
+    it('number min should be false when number inferior to min', () => {
+        expect(numberValidation('6', { min: 7 })).to.equal(false);
     });
-    it('number min should be true when number superior to min', ()=>{
-        const numberValidation = require('../number');
-        expect(numberValidation('7', {min: 6})).to.equal(true);
+    it('number min should be true when number superior to min', () => {
+        expect(numberValidation('7', { min: 6 })).to.equal(true);
     });
-    it('number max should be false when number superior to max', ()=>{
-        const numberValidation = require('../number');
-        expect(numberValidation('12', {max: 7})).to.equal(false);
+    it('number max should be false when number superior to max', () => {
+        expect(numberValidation('12', { max: 7 })).to.equal(false);
     });
-    it('number max should be true when number inferior to max', ()=>{
-        const numberValidation = require('../number');
-        expect(numberValidation('7', {max: 7})).to.equal(true);
+    it('number max should be true when number inferior to max', () => {
+        expect(numberValidation('7', { max: 7 })).to.equal(true);
     });
 });
