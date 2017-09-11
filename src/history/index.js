1911976641dae0b47741258bc13872f91a47c7fc
Bazire Houssin <bazire.houssin@kleegroup.com>
2017-09-11 17:09:23 +0200

    Fixing export for history

--- src/history/index.js
+++ src/history/index.js
@@ -1,29 +1,43 @@
 let _navigate, _back, _start;
 
 // Define the navigation functions depending on backbone or react-router
-export const setNavigationFunctions = (navigate, back, start) => {
+const setNavigationFunctions = (navigate, back, start) => {
     _navigate = navigate;
     _back = back;
     _start = start;
 }
 
-export const navigate = (...args) => {
-    if(!_navigate) {
+const navigate = (...args) => {
+    if (!_navigate) {
         throw new Error('react-router or backbone URL Navigation was badly given in the setNavigationFunctions()')
     }
     _navigate(...args);
 }
 
-export const back = (...args) => {
-    if(!_back){
+const back = (...args) => {
+    if (!_back) {
         throw new Error('react-router or backbone Previous Page Navigation was badly given in the setNavigationFunctions()')
     }
     _back(...args);
 }
 
-export const start = (...args) => {
-    if(!_start) {
+const start = (...args) => {
+    if (!_start) {
         throw new Error('Backbone start router was badly given in the setNavigationFunctions()')
     }
     _start(...args);
+}
+
+export {
+    setNavigationFunctions,
+    navigate,
+    back,
+    start
+}
+
+export default {
+    setNavigationFunctions,
+    navigate,
+    back,
+    start
 }
